f645be99b3bf10ec1601f9ae356da6399ae7b54e
Hugh Jones <hughjones@libreworx.com>
2015-02-22 16:06:48 -0500

    SPA Sample Racing Angular

---
+++ F1RaceFeed/F1RaceFeed/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace F1RaceFeed.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}

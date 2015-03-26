de6f260eac08c0135e2d5c3712558edc3a9d64cd
Hugh Jones <hughjones@libreworx.com>
2015-03-26 15:24:14 -0400

    done

--- jonesh-FincialPortal/AngularTemplate/Angular/app.js
+++ jonesh-FincialPortal/AngularTemplate/Angular/app.js
@@ -1,4 +1,27 @@
-﻿var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'LocalStorageModule', 'ui.grid']);
+﻿var app = angular.module('app', [
+
+    'ui.router',
+    'ui.bootstrap',
+        'ui.router',                // Angular flexible routing
+        'ngSanitize',               // Angular-sanitize
+        'ui.bootstrap',             // AngularJS native directives for Bootstrap
+        'angular-flot',             // Flot chart
+        'angles',                   // Chart.js
+        'angular-peity',            // Peity (small) charts
+        'cgNotify',                 // Angular notify
+        'angles',                   // Angular ChartJS
+        'ngAnimate',                // Angular animations
+        'ui.map',                   // Ui Map for Google maps
+        'ui.calendar',              // UI Calendar
+        'summernote',               // Summernote plugin
+        'ngGrid',                   // Angular ng Grid
+        'ui.tree',                  // Angular ui Tree
+        'bm.bsTour',                // Angular bootstrap tour
+        'datatables',               // Angular datatables plugin
+        'xeditable',                // Angular-xeditable
+        'ui.select',                 // AngularJS ui-select
+        'LocalStorageModule'
+]);
 
 
 app.config(['$stateProvider', '$locationProvider', '$httpProvider', '$urlRouterProvider', function 

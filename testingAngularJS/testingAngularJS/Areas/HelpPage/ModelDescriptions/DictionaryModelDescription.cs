a729375e5af1deeff7b16194366ee681720de434
Hugh Jones <hughjones@libreworx.com>
2015-04-06 08:28:24 -0400

    Angular tutorial

---
+++ testingAngularJS/testingAngularJS/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace testingAngularJS.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ testingAngularJS/testingAngularJS/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testingAngularJS.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}

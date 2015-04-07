d74169e9849a5f2b74e0b8fc210ccc19c7987e02
Hugh Jones <hughjones@libreworx.com>
2015-04-07 08:55:42 -0400

    SPA

---
+++ SPATesting/SPATesting/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SPATesting.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SPATesting/SPATesting/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SPATesting.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}

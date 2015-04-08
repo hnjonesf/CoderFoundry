4a4465e87cfea81c500af7e64190b5650f0fbb96
Hugh Jones <hughjones@libreworx.com>
2015-04-08 12:08:24 -0400

    Dashboard on FP, also demo homer

--- FincialPortal/AngularTemplate/Angular/transactions/controllers/TransactionsController.js
+++ FincialPortal/AngularTemplate/Angular/transactions/controllers/TransactionsController.js
@@ -7,22 +7,28 @@
         $scope.transactions = transactions;
 
         $scope.filterOptions = {
-    filterText: ''
-  };
+        filterText: ''
+        };
+
+        //$scope.categoryName = function (CategoryId) { return categories[transaction.CategoryId].Name; };
+        //$scope.categoryName = function () {
+        //    categories[transaction.CategoryId].Name;
+        //};
 
         //
+        $scope.selectedRows = [];
+
         $scope.gridOptions = {
             data: 'transactions',
+            showSelectionCheckbox: true,
+            selectedItems: $scope.selectedRows,
             showGroupPanel: true,
             filterOptions: $scope.filterOptions,
             jqueryUIDraggable: true,
             columnDefs: [{ field: 'Description', displayName: 'Description', width: "*" },
-                                    { field: 'Amount', displayName: 'Amount', width: "*" },
-                                    { field: 'Date', displayName: 'Date', width: "*" },
+                                    { field: 'Amount', displayName: 'Amount', width: "*", cellFilter: 'currency' },
+                                    { field: 'Date', displayName: 'Date', width: "*", cellFilter: "date:'MM-dd-yyyy'" },
                                     { field: 'CategoryId', cellClass: 'Category', width: "*" }]
         }
 
-        console.log(transactions);
-
     }]);
-

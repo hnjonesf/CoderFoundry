5bd36a81230a4505ef2a0d9f5cd51e2e8b077963
Hugh Jones <hughjones@libreworx.com>
2015-04-09 16:01:54 -0400

    fp

---
+++ FincialPortal/DatabaseBuilder/SQL/Security/Stored Procedures/GetAccountBalance.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[GetAccountBalance] 
+	@accountId int
+AS
+SELECT (Sum(t.Amount)+a.Balance) From [dbo].[Accounts]
+Accounts a 
+Join 
+AccountTransactions t 
+on 
+a.Id = t.AccountId
+WHERE AccountId = @accountId

4b4c644b8a195ff34970936eaf04f28ba8edb008
Hugh Jones <hughjones@libreworx.com>
2015-04-10 15:09:32 -0400

    working on sprocedures

---
+++ FincialPortal/DatabaseBuilder/SQL/Security/Stored Procedures/GetRecentTransByHousehold.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [dbo].[GetRecentTransByHousehold]
+	@householdId int
+AS
+BEGIN
+	SELECT TOP 5 CONVERT(varchar(10),[dbo].AccountTransactions.[Date],101) AS Date
+		, [dbo].Categories.[Name] AS Category
+		, [Security].Users.[Name] AS [User]
+		, [dbo].AccountTransactions.[SignedAmount] AS [Amount]
+		, [dbo].AccountTransactions.[Description]
+		, [dbo].Accounts.[Name] AS [Account]
+	FROM [dbo].AccountTransactions 
+		INNER JOIN [Security].Users
+			ON AccountTransactions.UpdatedByUserId=[Security].Users.Id
+		INNER JOIN Categories
+			ON AccountTransactions.CategoryId=Categories.Id
+		INNER JOIN Accounts
+			ON AccountTransactions.AccountId=Accounts.Id
+	WHERE [dbo].[Account].HouseHold = @householdId
+	ORDER BY [dbo].AccountTransactions.[Date] DESC, [dbo].AccountTransactions.[Id] DESC
+END
+GO

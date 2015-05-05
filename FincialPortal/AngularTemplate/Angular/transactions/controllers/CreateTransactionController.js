a679e7ff5b7ab7ef461a7b6b9a743cfc0631d709
Hugh Jones <hughjones@libreworx.com>
2015-05-05 07:23:41 -0400

    FP: add datepicker to create

--- FincialPortal/AngularTemplate/Angular/transactions/controllers/CreateTransactionController.js
+++ FincialPortal/AngularTemplate/Angular/transactions/controllers/CreateTransactionController.js
@@ -28,3 +28,40 @@
 
     }]);
 
+angular.module('app')
+.controller('DatepickerController', function ($scope) {
+    $scope.today = function() {
+        $scope.transaction.Date = new Date();
+    };
+    $scope.today();
+
+    $scope.clear = function () {
+        $scope.transaction.Date = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
+});
+

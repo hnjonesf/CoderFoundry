e13cf8af838729aa10b59df055a9ba81958aea53
Hugh Jones <hughjones@libreworx.com>
2015-05-21 13:04:29 -0400

    AngularJS Testing

---
+++ WebApplication1/WebApplication1/Angular/app.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('app', [])
+     
+
+.controller('mainController', function ($scope) {
+    $scope.phones = [
+      {
+          'name': 'Nexus S',
+          'snippet': 'Fast just got faster with Nexus S.'
+      },
+      {
+          'name': 'Motorola XOOM™ with Wi-Fi',
+          'snippet': 'The Next, Next Generation tablet.'
+      },
+      {
+          'name': 'MOTOROLA XOOM™',
+          'snippet': 'The Next, Next Generation tablet.'
+      },
+            {
+                'name': 'Jones Varooom™',
+                'snippet': 'Best in class, futeristic, phone.'
+            }
+    ];
+});

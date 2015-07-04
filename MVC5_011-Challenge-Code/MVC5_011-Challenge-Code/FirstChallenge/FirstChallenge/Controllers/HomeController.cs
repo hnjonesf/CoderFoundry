681e66672dc5e8cca599706ebd421b97e6ebccfb
Hugh Jones <hughjones@libreworx.com>
2015-07-04 10:57:56 -0400

    Tutorial MVC5

---
+++ MVC5_011-Challenge-Code/MVC5_011-Challenge-Code/FirstChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            return View(comics);
+        }
+
+        // Get: Details/5
+        public ActionResult Detail(int id) 
+        {
+            var comics = ComicBookManager.GetComicBooks();
+            var comic = comics.FirstOrDefault(c => c.ComicBookId == id);
+
+            return View(comic);
+        
+        }
+    }
+}

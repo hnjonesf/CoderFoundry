6e574b3c4f36a94efe1223087a6bf8dbf4dbbdb5
Hugh Jones <hughjones@libreworx.com>
2015-07-07 15:32:32 -0400

    Tech Testing: setup and published.

---
+++ TechTesting/TechTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechTesting.Startup))]
+namespace TechTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

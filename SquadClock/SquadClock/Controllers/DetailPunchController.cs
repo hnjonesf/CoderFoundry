6518631b135d78e508918a42f54585f63827f2f0
Hugh Jones <hughjones@libreworx.com>
2015-08-22 13:40:48 -0400

    ssl EmployeePunch Detail

---
+++ SquadClock/SquadClock/Controllers/DetailPunchController.cs
@@ -0,0 +1,27 @@
+﻿using SquadClock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SquadClock.Controllers
+{
+    public class DetailPunchController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: DetailPunch/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+    }
+}

0f5da1db1d8661f3910c85df65dba4da47d4bd2f
Hugh Jones <hughjones@libreworx.com>
2015-09-02 11:01:21 -0400

    LearningWEBForms

---
+++ LearningC/LearningC/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace LearningC.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ LearningC/LearningC/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearningC.Startup))]
+namespace LearningC
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}

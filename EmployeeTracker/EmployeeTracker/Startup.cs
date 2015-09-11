f869ad0b5b53d719e0810889b738d24bdaf61058
Hugh Jones <hughjones@libreworx.com>
2015-09-11 15:49:34 -0400

    scott allen

---
+++ EmployeeTracker/EmployeeTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeTracker.Startup))]
+namespace EmployeeTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

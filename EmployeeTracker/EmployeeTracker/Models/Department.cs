6a093a06a9aa9118dc4faa7a5a6c802ee596ec78
Hugh Jones <hughjones@libreworx.com>
2015-09-11 15:49:34 -0400

    scott allen

---
+++ EmployeeTracker/EmployeeTracker/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeTracker.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}

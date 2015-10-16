0178552ade0e8fa9e8fd49dc1544e5a69033c13f
Hugh Jones <hughjones@libreworx.com>
2015-10-16 07:54:44 -0400

    aaag

---
+++ AccountsAtAGlance/AccountsAtAGlance/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountsAtAGlance.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Trim { get; set; }
+        public double Cost { get; set; }
+    }
+}

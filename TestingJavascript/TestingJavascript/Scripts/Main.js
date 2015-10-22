08aa49697c4e5d6c47f2cf8357c9397f1d34f8a3
Hugh Jones <hughjones@libreworx.com>
2015-10-22 13:21:58 -0400

    testing so i can work from home

---
+++ TestingJavascript/TestingJavascript/Scripts/Main.js
@@ -0,0 +1,31 @@
+﻿////site javascript
+
+//if (window.jQuery) {
+//    // jQuery is loaded 
+//    alert("loaded");
+//} else {
+//    // jQuery is not loaded
+//    alert("not so fast");
+//}
+
+var constant = 32;
+
+function celsiusToFarenheit(temperature)
+{
+    var farenheitTemp = temperature * 9 / 5 + constant;
+    return farenheitTemp;
+}
+input = 33;
+output1.innerHTML = " degrees F";
+
+function hypo(a,b)
+{
+    function square(x)
+    {
+        return x * x;
+    }
+
+    return Math.sqrt(square(a) + square(b));
+}
+
+output2.innerHTML = hypo(9,9);
